1c02420885f06a4b10926380d59089b4b1d73ddd
ranisans <ranisans@yahoo.com>
2021-01-11 10:39:15 +0300

    set App styles to scroll task if task window is too big

--- src/components/PageWrapper/index.tsx
+++ src/components/PageWrapper/index.tsx
@@ -11,20 +11,27 @@ type TPageWrapper = (
 const useStyles = makeStyles(() => ({
   container: {
     height: "100%",
-    display: "grid",
-    gridTemplateRows: "auto auto 1fr",
+    display: "flex",
+    flexDirection: "column",
     gridGap: 10,
+    overflow: "hidden",
+    flex: 1,
   },
   title: {
     maxWidth: 450,
     fontWeight: 600,
     margin: "auto",
+    flex: "0 0 auto",
+  },
+  link: {
+    flex: "0 0 auto",
   },
   pageContainer: {
     boxSizing: "border-box",
     height: "100%",
-    overflowY: "auto",
-    paddingTop: 50,
+    overflow: "auto",
+    margin: "50px 0",
+    flex: 1,
   },
 }));
 
@@ -37,7 +44,9 @@ const PageWrapper: TPageWrapper = (taskName, taskLink, Page) => {
         <Typography variant="h2" className={styles.title}>
           {taskName}
         </Typography>
-        <Link href={taskLink}>Link to task</Link>
+        <Link href={taskLink} className={styles.link}>
+          Link to task
+        </Link>
         <div className={styles.pageContainer}>
           <Page />
         </div>
