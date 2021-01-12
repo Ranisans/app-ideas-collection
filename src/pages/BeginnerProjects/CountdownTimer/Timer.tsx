7db741a1f359e3e0ee779f74169f9ac7df975c4b
Rani <ranisans@yahoo.com>
2021-01-12 20:53:01 +0300

    add alert when wrong date remove React.StrictMode add Timer realization

--- src/pages/BeginnerProjects/CountdownTimer/Timer.tsx
+++ src/pages/BeginnerProjects/CountdownTimer/Timer.tsx
@@ -1,14 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { TextField } from "@material-ui/core";
 
 export interface ITimer {
   title: string;
   date: Date;
 }
 
-const Timer: React.FC<ITimer> = ({ title, date }: ITimer) => {
+export interface ITimerExt extends ITimer {
+  currentDate: Date;
+}
+
+interface IDifference {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+const secondInMinute = 60;
+const secondInHour = 60 * secondInMinute;
+const secondInDay = secondInHour * 24;
+
+const dateDiff = (currentDate: Date, dateEnd: Date): IDifference | null => {
+  if (currentDate >= dateEnd) return null;
+  let difference = Math.abs(currentDate.valueOf() - dateEnd.valueOf()) / 1000;
+
+  const days = Math.floor(difference / secondInDay);
+  difference -= days * secondInDay;
+  const hours = Math.floor(difference / secondInHour);
+  difference -= hours * secondInHour;
+  const minutes = Math.floor(difference / secondInMinute);
+  const seconds = Math.floor(difference - minutes * secondInMinute);
+
+  return { days, hours, minutes, seconds };
+};
+
+const Timer: React.FC<ITimerExt> = ({
+  title,
+  date,
+  currentDate,
+}: ITimerExt) => {
+  const [data, setData] = useState<IDifference | null>(null);
+
+  useEffect(() => {
+    const diff = dateDiff(currentDate, date);
+    setData(diff);
+  }, [date, currentDate]);
+
   return (
     <div>
-      <div>{title}</div>
+      <p>{title}</p>
+      {data ? (
+        <div>
+          <TextField
+            className="countdown_timer-field"
+            inputProps={{
+              readOnly: true,
+            }}
+            label="Days"
+            value={data.days}
+          />
+          <TextField
+            className="countdown_timer-field"
+            inputProps={{
+              readOnly: true,
+            }}
+            label="Hours"
+            value={data.hours}
+          />
+          <TextField
+            className="countdown_timer-field"
+            inputProps={{
+              readOnly: true,
+            }}
+            label="Minutes"
+            value={data.minutes}
+          />
+          <TextField
+            className="countdown_timer-field"
+            inputProps={{
+              readOnly: true,
+            }}
+            label="Seconds"
+            value={data.seconds}
+          />
+        </div>
+      ) : (
+        <p> Finished! </p>
+      )}
     </div>
   );
 };
--- src/pages/BeginnerProjects/CountdownTimer/index.tsx
+++ src/pages/BeginnerProjects/CountdownTimer/index.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "date-fns";
 import DateFnsUtils from "@date-io/date-fns";
-import { Button, TextField } from "@material-ui/core";
+import { Button, TextField, Snackbar } from "@material-ui/core";
 import { MuiPickersUtilsProvider, DateTimePicker } from "@material-ui/pickers";
+import MuiAlert, { AlertProps } from "@material-ui/lab/Alert";
 
 import PageWrapper from "../../../components/PageWrapper";
 import Timer, { ITimer } from "./Timer";
@@ -13,10 +14,16 @@ const title = "Countdown Timer";
 const taskLink =
   "https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Countdown-Timer-App.md";
 
+function Alert(props: AlertProps) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 const CountdownTimer: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
   const [name, setName] = useState("");
   const [timers, setTimers] = useState<ITimer[]>([]);
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
 
   const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
@@ -29,6 +36,10 @@ const CountdownTimer: React.FC = () => {
 
   const handleSetTimer = () => {
     if (!name) return;
+    if (selectedDate <= currentDate) {
+      setOpen(true);
+      return;
+    }
     setTimers([
       ...timers,
       {
@@ -36,7 +47,28 @@ const CountdownTimer: React.FC = () => {
         date: selectedDate,
       },
     ]);
+    setName("");
+    setSelectedDate(new Date());
   };
+
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    const increaseDate = () => {
+      setCurrentDate(new Date());
+    };
+    const interval = setInterval(increaseDate, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div className="countdown_timer-container">
@@ -67,15 +99,22 @@ const CountdownTimer: React.FC = () => {
           Start
         </Button>
       </div>
-      <div>
-        {timers.map((timerData) => (
+      <div className="countdown_timer-timers_block">
+        {timers.map((timerData, index) => (
           <Timer
-            key={`${timerData.title}-${timerData.date}`}
+            // eslint-disable-next-line react/no-array-index-key
+            key={`${timerData.title}-${timerData.date}-${index}`}
             title={timerData.title}
             date={timerData.date}
+            currentDate={currentDate}
           />
         ))}
       </div>
+      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error">
+          Wrong Date!
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
