e15a5abeb396dcc94c3871cd0f9766cbf16e7708
Rani <ranisans@yahoo.com>
2021-01-14 20:36:51 +0300

    feat add DollarsToCents logic

--- src/pages/BeginnerProjects/DollarsToCents/index.tsx
+++ src/pages/BeginnerProjects/DollarsToCents/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { TextField } from "@material-ui/core";
+import { Button, TextField } from "@material-ui/core";
 
 import PageWrapper from "../../../components/PageWrapper";
 
@@ -9,10 +9,30 @@ const title = "Dollars To Cents";
 const taskLink =
   "https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Dollars-To-Cents-App.md";
 
+const CENTS_IN_DOLLAR = 100;
+const QUARTER = 25;
+const DIME = 10;
+const NICKEL = 5;
+
+interface ICents {
+  quarters: number;
+  dimes: number;
+  nickels: number;
+  pennies: number;
+}
+
+const defaultCents: ICents = {
+  quarters: 0,
+  dimes: 0,
+  nickels: 0,
+  pennies: 0,
+};
+
 const DollarsToCents: React.FC = () => {
   const dollarPattern = /^\d+[.]?([\d*]{0,2})$/;
 
   const [dollarValue, setDollarValue] = useState("");
+  const [cents, setCents] = useState<ICents>(defaultCents);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
@@ -21,46 +41,69 @@ const DollarsToCents: React.FC = () => {
     }
   };
 
+  const calculateCents = (num: number) => {
+    let totalCents = Math.round(num * CENTS_IN_DOLLAR);
+    const quarters = Math.floor(totalCents / QUARTER);
+    totalCents -= quarters * QUARTER;
+    const dimes = Math.floor(totalCents / DIME);
+    totalCents -= dimes * DIME;
+    const nickels = Math.floor(totalCents / NICKEL);
+    const pennies = totalCents - nickels * NICKEL;
+    setCents({ quarters, dimes, nickels, pennies });
+  };
+
+  const handleCalculate = () => {
+    if (dollarValue) {
+      const num = parseFloat(dollarValue);
+      if (num > 0) {
+        calculateCents(num);
+      }
+    }
+  };
+
   return (
     <div className="dollars_to_cents-container">
       <div className="dollars_to_cents-input_block">
         <TextField
-          className="dollars_to_cents-base-input"
           label="Value in dollars"
           variant="outlined"
           value={dollarValue}
           onChange={handleChange}
         />
+
+        <Button variant="contained" color="primary" onClick={handleCalculate}>
+          Calculate
+        </Button>
       </div>
-      <div className="dollars_to_cents-output_block">
+      <div className="dollars_to_cents-input_block">
         <TextField
-          className="dollars_to_cents-result_input"
           label="Quarters"
           variant="outlined"
+          value={cents.quarters}
           inputProps={{
             readOnly: true,
           }}
         />
         <TextField
-          className="dollars_to_cents-result_input"
           label="Dimes"
           variant="outlined"
+          value={cents.dimes}
           inputProps={{
             readOnly: true,
           }}
         />
         <TextField
-          className="dollars_to_cents-result_input"
           label="Nickels"
           variant="outlined"
+          value={cents.nickels}
           inputProps={{
             readOnly: true,
           }}
         />
         <TextField
-          className="dollars_to_cents-result_input"
           label="Pennies"
           variant="outlined"
+          value={cents.pennies}
           inputProps={{
             readOnly: true,
           }}
