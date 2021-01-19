2da3dd70bdbc36811852499de0fa43310e157312
ranisans <ranisans@yahoo.com>
2021-01-19 10:21:47 +0300

    feat: add AlertError to FlipImage add add changing image URL and checking if the image is exist

--- src/pages/BeginnerProjects/FlipImage/FlipImage.tsx
+++ src/pages/BeginnerProjects/FlipImage/FlipImage.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, TextField } from "@material-ui/core";
 
+import ImageBlock from "./ImageBlock";
 import PageWrapper from "../../../components/PageWrapper";
 
 import "./index.scss";
-import ImageBlock from "./ImageBlock";
+import AlertError from "../../../components/AlertError";
 
 const title = "Flip Image";
 const taskLink =
@@ -11,14 +13,61 @@ const taskLink =
 
 const tempImage =
   "https://www.jquery-az.com/wp-content/uploads/2015/12/2.1-HTML-img-src.jpg";
+const timeout = 5000;
 
 const FlipImage: React.FC = () => {
+  const [urlValue, setURLValue] = useState("");
+  const [url, setURL] = useState(tempImage);
+  const [open, setOpen] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setURLValue(value);
+  };
+
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const checkImageURL = () => {
+    const img = new Image();
+    const timer = setTimeout(() => {
+      img.src = "//!!!!/noexist.jpg";
+      setOpen(true);
+    }, timeout);
+    img.onerror = () => {
+      clearTimeout(timer);
+      setOpen(true);
+    };
+    img.onload = () => {
+      clearTimeout(timer);
+      setURL(urlValue);
+    };
+    img.src = urlValue;
+  };
+
   return (
     <div className="flip_image-container">
-      <ImageBlock top left imageURL={tempImage} />
-      <ImageBlock top left={false} imageURL={tempImage} />
-      <ImageBlock top={false} left imageURL={tempImage} />
-      <ImageBlock top={false} left={false} imageURL={tempImage} />
+      <div className="flip_image-url_block">
+        <TextField label="Image URL" value={urlValue} onChange={handleChange} />
+        <Button variant="contained" color="primary" onClick={checkImageURL}>
+          Load
+        </Button>
+      </div>
+      <div className="flip_image-image_container_block">
+        <ImageBlock top left imageURL={url} />
+        <ImageBlock top left={false} imageURL={url} />
+        <ImageBlock top={false} left imageURL={url} />
+        <ImageBlock top={false} left={false} imageURL={url} />
+      </div>
+      <AlertError
+        open={open}
+        handleClose={handleClose}
+        text="Wrong Image URL!"
+      />
     </div>
   );
 };
